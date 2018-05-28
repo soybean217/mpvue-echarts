d390a618785a96409b20bacb3c2aabb27f485335
soybean217 <13565644@qq.com>
2018-05-28 16:48:26 +0800

    file sync

---
+++ src/utils/api.js
@@ -0,0 +1,85 @@
+import request from './request'
+import md5 from 'js-md5'
+
+
+/**
+ * 榜单：
+ * Top 250：
+ *  接口地址: top250
+ *  要求权限：movie_basic_r
+ *  可传参数：
+ *    @params {number} start=0 开始索引
+ *    @params {number} count=20 获取数量
+ *
+ * 北美票房榜：
+ *  接口地址：us_box
+ *  要求权限：movie_basic_r
+ *
+ * 正在上映：
+ *  接口地址: in_theaters
+ *  要求权限：movie_premium_r
+ *  可传参数：
+ *    @params {number} start=0 开始索引
+ *    @params {count} count=20 获取数量
+ *    @params {string} city=北京 城市
+ *
+ * 即将上映：
+ *  接口地址: coming_soon
+ *  要求权限：movie_premium_r
+ *  可传参数：
+ *    @params {number} start=0 开始索引
+ *    @params {number} count=20 获取数量
+ *
+ * 口碑榜 -> weekly （movie_advance_r）
+ * 新片榜 -> new_movies（movie_advance_r）
+ */
+export function getBoardData ({ board = 'top250', page = 1, count = 20, city = '北京', search = '' } = {}) {
+  let params = {}
+  if (board !== 'us_box') {
+    params.start = (page - 1) * count
+    params.count = count
+  }
+  if (board === 'in_theaters') {
+    params.city = city
+  }
+  if (board === 'search') {
+    params.q = search
+  }
+  return request.get(`/${board}`, params)
+}
+export function userLogin ({ userName = '测试账户', password = '888888'} = {}) {
+  // var formData = new FormData();
+  // formData.append('account',userName)
+  // formData.append('Password',md5(password))
+  let params = {}
+  params.account = userName
+  // params.Password = password
+  params.Password = md5(password)
+  console.log('params',json2Form(params))
+  return request.post(`/langrh/mobile/mobile!login.action`, json2Form(params))
+}
+
+function json2Form(json) {
+
+  var str = [];
+
+  for (var p in json) {
+
+    str.push(encodeURIComponent(p) + "=" + encodeURIComponent(json[p]));
+
+  }
+
+  return str.join("&");
+
+}
+
+/**
+ * 获取电影条目信息：
+ *  接口地址: subject/:id
+ *  要求权限：movie_basic_r
+ *  可传参数：
+ *    @params {number} id 电影id
+ */
+export function getMovieData (id) {
+  return request.get(`/subject/${id}`)
+}
